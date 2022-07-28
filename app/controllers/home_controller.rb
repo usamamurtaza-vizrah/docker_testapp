c7322bd6dcabb26ed87baf6859847dd33bfe689e
usama-murtaza0 <usamamurtaza0@gmail.com>
2022-07-28 23:32:11 +0500

    changed from table to modal

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,8 +4,39 @@ class HomeController < ApplicationController
 
   def index
     if params[:country_name] 
-      country = Country.find_by_name(params[:country_name])
-      render json: country
+      country_data = Country.find_by_name(params[:country_name])
+      data = [
+        {
+          key: 'Name',
+          value: country_data.name
+        },{
+          key: 'Name ES',
+          value: country_data.name_es
+        },{
+          key: 'Name AR',
+          value: country_data.name_ar
+        },{
+          key: 'Name FR',
+          value: country_data.name_fr
+        },{
+          key: 'Income Group',
+          value: country_data.income_group
+        },{
+          key: 'Economy',
+          value: country_data.economy
+        },{
+          key: 'Woe Note',
+          value: country_data.woe_note
+        },{
+          key: 'GDP Year',
+          value: country_data.gdp_year
+        },{
+          key: 'GDP MD',
+          value: country_data.gdp_md
+        }
+      ]
+
+      render json: data
     end
   end
   
