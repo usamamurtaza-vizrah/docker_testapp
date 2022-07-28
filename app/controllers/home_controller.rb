6399910b9b9224a3598a70e39e7cee089ecaf050
usama-murtaza0 <usamamurtaza0@gmail.com>
2022-07-28 22:25:52 +0500

    populating countries data with json

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,17 @@
 class HomeController < ApplicationController
+  skip_before_action :verify_authenticity_token, :only => [:index]
+  before_action :country_params, :only => [:index]
+
   def index
+    if params[:country_name] 
+      country = Country.find_by_name(params[:country_name])
+      render json: country
+    end
+  end
+  
+  private
+
+  def country_params
+    params.permit(:country_name)
   end
 end
